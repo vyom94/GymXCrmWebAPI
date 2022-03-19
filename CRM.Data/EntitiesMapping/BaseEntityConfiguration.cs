0aa202fc1accdec25de54e14352c361ef925cbbf
vyom94 <vyom94@gmail.com>
2022-03-20 01:20:08 +0530

    commit

---
+++ CRM.Data/EntitiesMapping/BaseEntityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using CRM.Framework.DataContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.Data.EntitiesMapping
+{
+    public class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).ValueGeneratedOnAdd();
+            builder.Property(e => e.CompanyId).IsRequired().HasColumnType("smallint");
+            builder.Property(e => e.CreatedBy).IsRequired().HasMaxLength(25);
+            builder.Property(e => e.CreatedAt).IsRequired().HasColumnType("datetime2");
+            builder.Property(e => e.UpdatedBy).HasMaxLength(25);
+            builder.Property(e => e.UpdatedAt).HasColumnType("datetime2");
+            builder.Property(e => e.Active).IsRequired().HasColumnType("bit");
+            builder.Property(e => e.Deleted).IsRequired().HasColumnType("bit");
+            //builder.Property(e => e.ChgTimeStamp).IsRowVersion().ValueGeneratedOnAddOrUpdate();//.IsRequired();
+        }
+    }
+}
