c78f971022faeea84ccf4c6d384815d34b8e3c83
vyom94 <vyom94@gmail.com>
2022-03-21 14:26:34 +0530

    aaa
    commit changes

---
+++ CRM.Models/User/User.cs
@@ -0,0 +1,15 @@
+﻿using CRM.Framework.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.Models.User
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; }
+        public  string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
